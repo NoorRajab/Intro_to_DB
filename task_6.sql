5ad980eab8a9be4c032edc2bf0956ffc2405ab83
Noor_Rajab <rajabnoor115@gmail.com>
2025-10-19 19:00:51 +0300

    updated files

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 
-USE ALX_BOOK_STORE;
+USE alx_book_store;
 
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES 
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
